3e4d36766d402591e7ecb7dfeaba2233da22a251
Javier Charry <javierandrescharry@gmail.com>
2024-11-09 11:21:17 -0500

    first commit

---
+++ src/test/java/com/NoBugs/nobugs_exception/NoBugsExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NoBugs.nobugs_exception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoBugsExceptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
